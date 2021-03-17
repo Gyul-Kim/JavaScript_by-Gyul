a496f0029bdaaf14eef442bc08487e5f65a72dab
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2021-03-18 01:23:52 +0900

    자바스크립트로 게임 구현하기

---
+++ 04_Reaction Game/09-2.call.js
@@ -0,0 +1,26 @@
+// 호출 스택
+function d() {
+    console.log('d';)
+}
+
+function e() {
+    console.log('e';)
+}
+
+function a() {
+    function b() {
+        function c() {
+            console.log('c');
+        }
+        c();
+        console.log('b');
+    }
+    b();
+    console.log('a');
+}
+
+d();
+e();
+a();
+
+// Last In, First Out 후입선출 구조!
